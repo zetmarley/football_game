0412718a468f1efee5be5ccd13d10733e1195be7
vvnfrd <zetmarley@outlook.com>
2025-05-14 03:06:34 +0700

    some fixes

--- main/views.py
+++ main/views.py
@@ -42,27 +42,34 @@ class GameView(TemplateView):
 
     def post(self, request, *args, **kwargs):
         context = super().get_context_data(**kwargs)
-        if not get_session_values(request):
-            set_session_value(request)
-            return render(request, self.template_name, context)
-        else:
-            selected_player = Player.objects.get(name=request.POST.get('footballer'))
-            selected_players = request.session.get('user')['selected_players']
-            if request.session['user']['hidden_player'] != selected_player:
-                selected_players.append(selected_player)
-                request.session['user']['selected_players'] = selected_players
-                request.session['user']['attempts'] -= 1
-                request.session.save()
+        try:
+            if not get_session_values(request):
+                set_session_value(request)
+                return render(request, self.template_name, context)
             else:
-                selected_players.append(selected_player)
-                request.session['user']['selected_players'] = selected_players
-                request.session.save()
+                selected_player = Player.objects.get(name=request.POST.get('footballer'))
+                selected_players = request.session.get('user')['selected_players']
+                if request.session['user']['hidden_player'] != selected_player:
+                    selected_players.append(selected_player)
+                    request.session['user']['selected_players'] = selected_players
+                    request.session['user']['attempts'] -= 1
+                    request.session.save()
+                else:
+                    selected_players.append(selected_player)
+                    request.session['user']['selected_players'] = selected_players
+                    request.session.save()
 
-            context['hidden_player'] = request.session['user']['hidden_player']
+                context['hidden_player'] = request.session['user']['hidden_player']
+                context['selected_players'] = request.session['user']['selected_players']
+                context['attempts'] = request.session['user']['attempts']
+                context['show_dialog '] = request.session['user']['show_dialog']
+
+                return render(request, self.template_name, context)
+        except Player.DoesNotExist:
             context['selected_players'] = request.session['user']['selected_players']
+            context['hidden_player'] = request.session['user']['hidden_player']
             context['attempts'] = request.session['user']['attempts']
             context['show_dialog '] = request.session['user']['show_dialog']
-
             return render(request, self.template_name, context)
 
 
